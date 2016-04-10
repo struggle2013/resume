38891003784bb675a4b3931110730cb2b26eff7b
luo <2292946166@qq.com>
2016-04-10 23:38:26 +0800

    再次修改

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
-$servername="SAE_MYSQL_HOST_M:SAE_MYSQL_PORT ";
-$username="SAE_MYSQL_USER";
-$password="SAE_MYSQL_PASS";
+$servername=SAE_MYSQL_HOST_M . ":" . SAE_MYSQL_PORT ;
+$username=SAE_MYSQL_USER;
+$password=SAE_MYSQL_PASS;
 $con=@mysql_connect($servername,$username,$password)or die("连接mysql数据库失败");
 
  @mysql_selectdb("resume",$con) or die("resume数据库打开失败");
