da9e767e0b4ffe7f536f05ca47113297c6ec8e78
luo <2292946166@qq.com>
2016-04-10 23:40:04 +0800

    再次修改

--- conn.php
+++ conn.php
@@ -4,6 +4,6 @@ $username=SAE_MYSQL_USER;
 $password=SAE_MYSQL_PASS;
 $con=@mysql_connect($servername,$username,$password)or die("连接mysql数据库失败");
 
- @mysql_selectdb("resume",$con) or die("resume数据库打开失败");
+ @mysql_selectdb("app_resume201604",$con) or die("resume数据库打开失败");
  mysql_set_charset("utf8");
 ?>    
